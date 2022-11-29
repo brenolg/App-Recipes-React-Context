e24c1992ebf9d87f9d80898c4519fb1adc46f20a
Vitor <vbrenochacon@hotmail.com>
2022-11-29 17:52:10 -0300

    requisito 4: validação de login implementada

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginButtonValidation, setLoginButtonValidation] = useState(true);
+
+  useEffect(() => {
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const six = 6;
+    if (regex.test(email) && password.length > six) {
+      setLoginButtonValidation(false);
+    } else {
+      setLoginButtonValidation(true);
+    }
+  });
+
   return (
     <form>
       <label
@@ -10,6 +24,8 @@ function LoginForm() {
           id="email-input"
           type="text"
           data-testid="email-input"
+          value={ email }
+          onChange={ (e) => setEmail(e.target.value) }
         />
       </label>
       <label
@@ -19,11 +35,15 @@ function LoginForm() {
           id="password-input"
           type="password"
           data-testid="password-input"
+          value={ password }
+          onChange={ (e) => setPassword(e.target.value) }
         />
       </label>
       <button
         type="button"
         data-testid="login-submit-btn"
+        disabled={ loginButtonValidation }
+
       >
         Login
       </button>
