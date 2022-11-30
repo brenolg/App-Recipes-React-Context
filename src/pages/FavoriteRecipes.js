0f45b13cd530cb85a80b11da25d9692996c4200a
jaron morren <jaronmorren@gmail.com>
2022-11-30 10:42:03 -0300

    requisito 7 terminado sem rotas

---
+++ src/pages/FavoriteRecipes.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header';
+
+function FavoriteRecipes() {
+  return (
+    <div>
+      <Header pageTitle="Favorite Recipes">Favorite Recipes</Header>
+    </div>
+  );
+}
+
+export default FavoriteRecipes;
---
+++ src/pages/Profile.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header';
+
+function Profile() {
+  return (
+    <div>
+      <Header pageTitle="Profile">Profile</Header>
+    </div>
+  );
+}
+
+export default Profile;
