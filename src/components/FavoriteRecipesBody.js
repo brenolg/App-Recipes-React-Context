9549fd194399e614b77c66b2e49a4a90a3584936
Vitor <vbrenochacon@hotmail.com>
2022-12-01 17:53:17 -0300

    requisito 53: Função do Botão de compartilhar implementada

--- src/components/FavoriteRecipesBody.js
+++ src/components/FavoriteRecipesBody.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import copy from 'clipboard-copy';
 import shareIcon from '../images/shareIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 function FavoriteRecipesBody() {
   const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  const [flag, setFlags] = useState([]);
   useEffect(() => {
     setFavoriteRecipes(JSON.parse(localStorage.getItem('favoriteRecipes')));
+    setFlags(favoriteRecipes.map((recipe) => ({
+      [recipe.id]: false,
+    })));
   }, []);
   return (
     <>
@@ -51,7 +56,15 @@ function FavoriteRecipesBody() {
           </p>
           <button
             type="button"
+            onClick={ () => {
+              copy(`http://localhost:3000/${recipe.type}s/${recipe.id}`);
+              setFlags((current) => ({
+                ...current,
+                [recipe.id]: true,
+              }));
+            } }
           >
+            { flag[recipe.id] && ('Link copied!')}
             <img
               data-testid={ `${index}-horizontal-share-btn` }
               src={ shareIcon }
