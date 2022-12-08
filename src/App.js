0396dabb7648b056472e431eeda17c4b0c79924b
alexandrebelem21 <alexandrebelem21@gmail.com>
2022-12-08 15:57:04 -0300

    feat: Fix Routes SearchBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
 import Recipes from './pages/Recipes';
@@ -12,23 +11,12 @@ import FavoriteRecipes from './pages/FavoriteRecipes';
 function App() {
   return (
     <Switch>
-      <Route exact path="/done-recipes" component={ DoneRecipes } />
       <Route exact path="/" component={ Login } />
       <Route path="/meals" component={ Recipes } />
       <Route path="/drinks" component={ Recipes } />
       <Route path="/profile" component={ Profile } />
       <Route path="/favorite-recipes" component={ FavoriteRecipes } />
       <Route path="/done-recipes" component={ DoneRecipes } />
-      <div className="meals">
-        <span className="logo">TRYBE</span>
-        <object
-          className="rocksGlass"
-          type="image/svg+xml"
-          data={ rockGlass }
-        >
-          Glass
-        </object>
-      </div>
     </Switch>
   );
 }
