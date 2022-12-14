1a4a7fee20110d37237f584e90950b6f6bdafe44
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-12-14 14:16:44 -0300

    inciando CSS recipes

---
+++ src/images/drinksCatList.js
@@ -0,0 +1,14 @@
+import ordinaryDrink from './ordinaryDrink.svg';
+import cocktail from './cocktail.svg';
+import shake from './shake.svg';
+import otherDrink from './otherDrink.svg';
+import cocoa from './cocoa.svg';
+
+const drinksCatList = [
+  ordinaryDrink,
+  cocktail,
+  shake,
+  otherDrink,
+  cocoa];
+
+export default drinksCatList;
---
+++ src/images/mealsCatList.js
@@ -0,0 +1,14 @@
+import beef from './beef.svg';
+import breakfast from './breakfast.svg';
+import chicken from './chicken.svg';
+import dessert from './dessert.svg';
+import goat from './goat.svg';
+
+const drinksCatList = [
+  beef,
+  goat,
+  chicken,
+  breakfast,
+  dessert];
+
+export default drinksCatList;
