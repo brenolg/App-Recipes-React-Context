06cbd2d88ddd7e3c3e8f6d2b1577ff95a4ebf2e2
Vitor M. Alves <vtr.vma@hotmail.com>
2022-12-16 15:13:24 -0300

    feat: ajustes detalhes

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -2,20 +2,34 @@ import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Loading from '../components/Loading';
 
 function RecipesDetails() {
   const history = useHistory();
-  const [recipe, setRecipe] = useState();
+  const [recipe, setRecipe] = useState(); // guarda a receita
+  const [info, setInfo] = useState();
+  const [ingredients, setIngredients] = useState([]);
+  const { pathname } = history.location;
+  const pathSplit = pathname.split('/');
+  const type = pathSplit[1];
+
+  const removeEmptyAttrs = (objMapped) => {
+    Object.keys(objMapped).forEach((key) => {
+      if (objMapped[key] && typeof objMapped[key] === 'object') {
+        removeEmptyAttrs(objMapped[key]);
+        if (Object.keys(objMapped[key]).length === 0) { // se pai ficou sem filhos
+          delete objMapped[key]; // remove pai
+        }
+      } else if (objMapped[key] === null) delete objMapped[key];
+    });
+  };
 
   const fetchIdRecipe = async (url) => {
     const response = await fetch(url);
-    const data = await response.json();
-    setRecipe(data);
+    setRecipe(await response.json());
   };
 
   useEffect(() => {
-    const { pathname } = history.location;
-    const pathSplit = pathname.split('/');
     const id = pathSplit[2];
     console.log(pathSplit[1]);
     if (pathSplit[1] === 'meals') {
@@ -23,14 +37,56 @@ function RecipesDetails() {
     } else {
       fetchIdRecipe(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
     }
-
-    console.log(recipe);
+    // const i = recipe[type];
+    // console.log(i);
   }, []);
+  console.log(recipe);
   return (
     <div>
       <Header />
       <h1>Recipe Details</h1>
-      <Footer />
+      <div>
+        {
+          (recipe && type === 'meals') && (
+            recipe.meals.map((meal) => (
+              <div key={ meal.idMeal }>
+                <img
+                  src={ meal.strMealThumb }
+                  alt={ meal.strMeal }
+                  data-testid="recipe-photo"
+                />
+                <h3 data-testid="recipe-title">{meal.strMeal}</h3>
+                <h4 data-testid="recipe-category">{`Categoria: ${meal.strCategory}`}</h4>
+                <ul>
+                  <li data-testid="1-ingredient-name-and-measure">
+                    {meal.strIngredient1}
+                  </li>
+                  <li data-testid="1-ingredient-name-and-measure">
+                    {meal.strIngredient1}
+                  </li>
+                </ul>
+              </div>
+            ))
+          )
+        }
+        {/* {
+          !recipe ? <Loading /> : (
+            // console.log(recipe.meals)
+            recipe.meals.map((meal) => (
+              <div key={ meal.idMeal }>
+                <img
+                  src={ meal.strMealThumb }
+                  alt={ meal.strMeal }
+                  data-testid="recipe-photo"
+                />
+                <h3 data-testid="recipe-title">{meal.strMeal}</h3>
+                <h4 data-testid="recipe-category">{meal.strCategory}</h4>
+              </div>
+            ))
+          )
+        } */}
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 }
